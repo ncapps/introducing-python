15e9227a6521f8ea12cd1d72bbff5c4bb56812bb
Nick Capps <nickcapps10@gmail.com>
2020-03-16 18:02:39 -0700

    Add ch2 data types examples

---
+++ ch2_data_types.py
@@ -0,0 +1,12 @@
+# 2.1 Assign the integer value 99 to the variable prince and print it
+prince = 99
+print(prince)
+
+#2.2 What type is the value 5?
+print("5 is a", type(5))
+
+#2.3 What type is the value 2.0?
+print("2.0 is a", type(2.0))
+
+#2.4 What type is the expression 5 + 2.0?
+print("5 + 2.0 is a", type(5 + 2.0))
